3f893f9de576eab33a646a37886975e5daf09f19
Marco Tuzza <marcotuzza@hotmail.it>
2024-01-24 18:44:06 +0100

    Adding light mode icon
    - Added Fontawesome
    - Added both light mode  and dark mode button
    - Adjusted the theme accordingly

--- main.js
+++ main.js
@@ -17,4 +17,13 @@ const themeToggle = document.getElementById('theme-toggle');
 
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
+    if (themeToggle.id === 'theme-toggle') {
+        themeToggle.id = 'theme-toggle-light';
+        var element = document.getElementById('theme-icon');
+        element.classList.replace('fa-sun', 'fa-moon');
+    } else {
+        themeToggle.id = 'theme-toggle';
+        var element = document.getElementById('theme-icon');
+        element.classList.replace('fa-moon', 'fa-sun');
+    }
 });
